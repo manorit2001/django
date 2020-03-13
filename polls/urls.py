233c64c966ee131506b10e34324b76934d156cfe
manorit chawdhry <manorit2001@gmail.com>
2020-03-13 23:36:43 +0530

    part 3 done

--- polls/urls.py
+++ polls/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[path('',views.index,name='index'),]
+urlpatterns=[
+path('',views.index,name='index'),
+path('<int:qid>/',views.detail,name='detail'),
+path('<int:qid>/results',views.results,name='results'),
+path('<int:qid>/vote',views.vote,name='vote'),
+path('<int:qid>/detail',views.detail,name='detail'),
+]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,50 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404,reverse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import question, choice
 
 # Create your views here.
-def index(r):
-    return HttpResponse("Hello world. You're at the polls index")
+
+
+def index(req):
+    ql = question.objects.all()
+    # return HttpResponse("Hello world. You're at the polls index")
+    context = {
+        "ql": ql
+    }
+    return render(req, 'polls/index.html', context=context)
+
+
+def results(req, qid):
+    cl = choice.objects.filter(question_id=qid)
+    context = {
+        "cl": cl,
+        "q": question.objects.get(id=qid)
+    }
+    return render(req, 'polls/results.html', context=context)
+
+
+def vote(req, qid):
+    print(req.POST)
+    q = get_object_or_404(question, id=qid)
+    print(q.choice_set.all())
+    try:
+        c = q.choice_set.get(choice_text=(req.POST['choice']))
+    except (KeyError):
+        return render(req, 'polls/detail.html', {
+            'q': q,
+            # "You did not select a valid choice",
+            'error_message': str(req.POST['choice'])+' not found'
+        })
+    else:
+        print(c)
+        print(dir(c))
+        c.votes += 1
+        c.save()
+    return HttpResponseRedirect(reverse('results', args=(qid,)))
+
+
+def detail(req, qid):
+    context = {
+        "q": question.objects.get(id=qid)
+    }
+    return render(req, 'polls/detail.html', context=context)
